ecfcf0aeed51aa0172ebaf8e5949a73ccb8a5666
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-06-11 00:10:05 +0600

    initial server created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Global Tongues server is running")
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
