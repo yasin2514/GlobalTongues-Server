ec1c4eac1ed651d483303da5e331e4944a183723
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-06-11 04:43:59 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
 app.use(express.json());
 
-// jwt middleware 
-const verifyJwt = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
-        return res.status(401).send({ error: true, message: "Unauthorized Access" });
-    }
-    const token = authorization.spilt(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ error: true, message: "Unauthorized Access" })
-        }
-        req.decoded = decoded;
-        next();
-    })
-}
+// jwt middleware
+// const verifyJWT = (req, res, next) => {
+//     const authorization = req.headers.authorization;
+//     if (!authorization) {
+//         return res.status(401).send({ error: true, message: "Unauthorized access" })
+//     }
+//     const token = authorization.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(401).send({ error: true, message: "unauthorized access" })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+
+// }
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -36,11 +38,37 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const userCollection = client.db('globalTongues').collection('users');
+
+        // jwt token apis
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+
+        // users apis
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const userExit = await userCollection.findOne(query);
+            if (userExit) {
+               return res.send({ message: "user already exits" })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
