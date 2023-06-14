232130b1b909c59809f1fc189d2afbf221a44c46
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-06-14 17:45:09 +0600

    verlcel deploy done

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const userCollection = client.db('globalTongues').collection('users');
         const classCollection = client.db('globalTongues').collection('classes');
@@ -330,7 +330,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection----------------------------database-------------
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
